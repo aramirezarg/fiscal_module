e64d19470e8aada110a4c1ee8c3289a7dcf67ac3
Alfredo <alfredrz2012@gmail.com>
2020-07-16 22:27:35 -0600

    Update to __version__ = '1.0.9'

--- fiscal_module/fiscal_module/doctype/fiscal_document/fiscal_document.py
+++ fiscal_module/fiscal_module/doctype/fiscal_document/fiscal_document.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from datetime import datetime
+from frappe.utils import date_diff
 from frappe import _
 
 
