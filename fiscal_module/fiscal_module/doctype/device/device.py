147aed4383f919743d28f8f03d2d23abceea8d67
Alfredo <alfredrz2012@gmail.com>
2020-08-04 15:59:43 -0600

    Update to __version__ = '1.2.5'

--- fiscal_module/fiscal_module/doctype/device/device.py
+++ fiscal_module/fiscal_module/doctype/device/device.py
@@ -115,12 +115,12 @@ class Device(Document):
 
     @staticmethod
     def set_identifier(device_id):
-        import datetime
-        expires = datetime.datetime.now() + datetime.timedelta(days=2)
+        # import datetime
+        # expires = datetime.datetime.now() + datetime.timedelta(days=2)
         frappe.cache().hset('device_id', frappe.session.user, device_id)
 
-        if hasattr(frappe.local, "cookie_manager"):
-            frappe.local.cookie_manager.set_cookie("device_id", device_id, expires=expires, httponly=False)
+        # if hasattr(frappe.local, "cookie_manager"):
+        #    frappe.local.cookie_manager.set_cookie("device_id", device_id, expires=expires, httponly=False)
 
         if frappe.get_value("Device", device_id) is None:
             doc = frappe.new_doc("Device")
